5327b503ac9ee6d68b202d3c624c8a3e4847bc96
Franco Alberto Cardillo <facardillo@gmail.com>
2022-03-17 15:22:43 +0000

    chestxray, nb

--- eddl_src/utils/mean_and_variance.py
+++ eddl_src/utils/mean_and_variance.py
@@ -15,31 +15,31 @@ from tqdm import tqdm
 # std: [[0.26261734 0.26261734 0.26261734]]
 #
 
+# NOTE: 
 img_size = 300
 augs = ecvl.SequentialAugmentationContainer([
            ecvl.AugResizeDim([img_size, img_size]),
            ecvl.AugToFloat32(divisor=255.0),
 ])
 
-def main(img_fld):
-    filenames = [join(img_fld, fn) for fn in os.listdir(img_fld) if fn.endswith(".png")]
+
+def flat_layout(img_fld, ext=".png"):
+    filenames = [join(img_fld, fn) for fn in os.listdir(img_fld) if fn.endswith(ext)]
     print(f"|images| = {len(filenames)}")
     m = Tensor.zeros( [1, 3] )
     s = Tensor.zeros( [1, 3] )
 
+    right = 30
     with tqdm(total=len(filenames)) as pbar:
         for i, fn in enumerate(filenames):
-            pbar.set_description(f"{os.path.basename(fn)}")
+            spaces = (right-len(os.path.basename(fn))) * " "
+            pbar.set_description(f"{os.path.basename(fn)}{spaces}")
             img = ecvl.ImRead(fn)
             augs.Apply(img)
             ecvl.RearrangeChannels(img, img, "cxy")
-            augs.Apply(img)
-            # ecvl.ImWrite("a.png", img)
             a = np.array(img, dtype=float)
             a = a.reshape([3, -1])  # img_size*img_size])
-            print(a.shape)
             m += np.sum(a, axis=1)
-            exit(1)
             s += np.sum(a**2, axis=1) 
             pbar.update(1)
         #<
@@ -52,6 +52,19 @@ def main(img_fld):
     std = np.sqrt(s)
     
     print(f"images: {len(filenames)}, pixels:{n_pixels}")
+    return m, s, std
+
+
+def main(img_fld, dataset):
+
+    m, s, std = 0, 0, 0
+    if dataset == "iu-chest":
+        m, s, std = flat_layout(img_fld, ".png")
+    elif dataset == "chest-xray8":
+        m, s, std = flat_layout(img_fld, ".png")
+    else:
+        assert False, f"unknown dataset {dataset}"
+
     print(f"mean: {m}")
     print(f"var:  {s}")
     print(f"std: {std}")
